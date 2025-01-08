59e0d02274333fa94f16ab2f284252e3986d645a
ParvathiSanjay <parvathi.gmys@gmail.com>
2025-01-08 13:54:50 +0530

    First commit

---
+++ src/ex_01_Java_Basic/Lab002_CommentLine.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basic;
+
+public class Lab002_CommentLine
+{
+    public static void main(String[] args)
+    {
+        System.out.println("single comment line");
+       // single line comment
+        git config --global user.name "ParvathiSanjay"
+
+    }
+}
---
+++ src/ex_01_Java_Basic/Singlecomment.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basic;
+
+public class Singlecomment {
+    public static void main(String[] args) {
+        System.out.println(" single line comment");
+        // example of single comment
+    }
+}
