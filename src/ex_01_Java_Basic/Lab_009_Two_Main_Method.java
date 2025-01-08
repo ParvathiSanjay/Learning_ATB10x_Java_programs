f36cfdfd96c2ea99925c7488cc054bf341fb84d5
ParvathiSanjay <parvathi.gmys@gmail.com>
2025-01-08 18:44:10 +0530

    two main method

---
+++ src/ex_01_Java_Basic/Lab_009_Two_Main_Method.java
@@ -0,0 +1,15 @@
+package ex_01_Java_Basic;
+
+public class Lab_009_Two_Main_Method {
+    public static void main(String[] args) {
+        System.out.println("two main method");
+//main();
+}
+
+  /* public static void main(int x) {
+      //  int y=x;
+        System.out.println(10);*/
+
+
+    }
+
