33c0dab626cfb962e427121832077020efe97c92
ParvathiSanjay <parvathi.gmys@gmail.com>
2025-01-16 20:02:58 +0530

    4 commit

---
+++ src/ex_02_Java_/lab010_add.java
@@ -0,0 +1,14 @@
+package ex_02_Java_;
+
+public class lab010_add {
+    public static void main(String[] args) {
+
+
+        int a = 10;
+        int b = 20;
+        System.out.println(a + b);
+        System.out.println(a-b);
+
+    }
+}
+
