5f2923199642aac1a75bced28c1fe817bc544fe5
ParvathiSanjay <parvathi.gmys@gmail.com>
2025-01-24 19:34:44 +0530

    increment and decrement  operators

---
+++ src/ex_03_Java_Operators/Decrement_operator.java
@@ -0,0 +1,15 @@
+package ex_03_Java_Operators;
+
+public class Decrement_operator {
+    public static void main(String[] args) {
+         int a =10;
+        int res=a--;
+        System.out.println(res);
+        System.out.println(a);
+
+        int b =10;
+        int res1=--b;
+        System.out.println(res1);
+        System.out.println(b);
+    }
+}
---
+++ src/ex_03_Java_Operators/increment_operators.java
@@ -0,0 +1,15 @@
+package ex_03_Java_Operators;
+
+public class increment_operators {
+    public static void main(String[] args) {
+       /* int a=10;
+        int res= a++;
+        System.out.println(res);
+        System.out.println(a);
+        */
+        int a=10;
+        int res= ++a;
+        System.out.println(res);
+        System.out.println(a);
+    }
+}
