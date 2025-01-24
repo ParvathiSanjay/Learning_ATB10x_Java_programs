c538c9d6a3bde6812d7f880ce345edc6dd66d7c2
ParvathiSanjay <parvathi.gmys@gmail.com>
2025-01-24 19:20:22 +0530

    logical operators

--- src/ex_03_Java_Operators/relational_operators.java
+++ src/ex_03_Java_Operators/relational_operators.java
@@ -2,10 +2,13 @@ package ex_03_Java_Operators;
 
 public class relational_operators {
     public static void main(String[] args) {
-        int a=20 , b=10;
+        int a=20 , b=20;
         System.out.println(a>b);
         System.out.println(a<b);
         System.out.println(a<=b);
+        System.out.println(a>=b);
+        System.out.println(a==b);
+        System.out.println(a!=b);
 
     }
 }
