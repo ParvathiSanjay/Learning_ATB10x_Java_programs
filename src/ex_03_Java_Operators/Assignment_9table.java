ca31d49c5547165b85c9fc5a81b29117caa67944
ParvathiSanjay <parvathi.gmys@gmail.com>
2025-01-24 19:58:21 +0530

    9 table

---
+++ src/ex_03_Java_Operators/Assignment_9table.java
@@ -0,0 +1,17 @@
+package ex_03_Java_Operators;
+
+public class Assignment_9table {
+    public static void main(String[] args) {
+        int a=9;
+        System.out.println("table of 9*1=" +a*1);
+        System.out.println("table of 9*1=" +a*2);
+        System.out.println("table of 9*1=" +a*3);
+        System.out.println("table of 9*1=" +a*4);
+        System.out.println("table of 9*1=" +a*5);
+        System.out.println("table of 9*1=" +a*6);
+        System.out.println("table of 9*1=" +a*7);
+        System.out.println("table of 9*1=" +a*8);
+        System.out.println("table of 9*1=" +a*9);
+        System.out.println("table of 9*1=" +a*10);
+    }
+}
---
+++ src/ex_03_Java_Operators/Shot_hand_operator.java
@@ -0,0 +1,19 @@
+package ex_03_Java_Operators;
+
+public class Shot_hand_operator {
+    public static void main(String[] args) {
+        int a=10;
+
+      a+=5; // a=a+5;
+
+        System.out.println(a);
+       a-=5;// a=a-5;
+        System.out.println(a);
+        a*=2;
+        System.out.println(a);
+
+
+        a/=2;
+        System.out.println(a);
+    }
+}
