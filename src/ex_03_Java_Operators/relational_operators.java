9c07e5348649d2786f1cbc6e60fba7040b698ca3
ParvathiSanjay <parvathi.gmys@gmail.com>
2025-01-24 19:03:49 +0530

    relational operators

---
+++ src/ex_03_Java_Operators/relational_operators.java
@@ -0,0 +1,11 @@
+package ex_03_Java_Operators;
+
+public class relational_operators {
+    public static void main(String[] args) {
+        int a=20 , b=10;
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a<=b);
+
+    }
+}
