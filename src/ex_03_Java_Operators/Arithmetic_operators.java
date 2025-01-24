286e88b277cc9da355d8cb1ea39b15450c2a2ef4
ParvathiSanjay <parvathi.gmys@gmail.com>
2025-01-24 14:24:34 +0530

    First commit

---
+++ src/ex_03_Java_Operators/Arithmetic_operators.java
@@ -0,0 +1,15 @@
+package ex_03_Java_Operators;
+
+public class Arithmetic_operators {
+    public static void main(String[] args) {
+        int a=20 ;
+        int b=10;
+        System.out.println("Sum of a and b:" + (a+b));
+        System.out.println("Sub of a and b:" + (a-b));
+        System.out.println("Multi of a and b:" + (a*b));
+        System.out.println("Division of a and b:" + (a/b));
+        System.out.println("Mod of a and b:" + (a%b));
+    }
+
+
+}
