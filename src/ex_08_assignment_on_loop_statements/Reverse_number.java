9a3dcc25c0a9b876e896666043c6099c09cd07a4
ParvathiSanjay <parvathi.gmys@gmail.com>
2025-03-11 20:05:40 +0530

    assignments of class8

--- src/ex_08_assignment_on_loop_statements/Reverse_number.java
+++ src/ex_08_assignment_on_loop_statements/Reverse_number.java
@@ -1,4 +1,27 @@
 package ex_08_assignment_on_loop_statements;
 
-public class Reverse_number {
+import java.util.Scanner;
+
+public class Reverse_number
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number = ");
+        int num = sc.nextInt();
+        int reversed = 0;
+     while (num!=0)
+     {
+         int digit = num % 10;
+         reversed = reversed * 10 + digit;
+         num /= 10;
+     }
+        System.out.println("Revesed number= " + reversed );
+     sc.close();
+
+
+
+    }
+
+
 }
