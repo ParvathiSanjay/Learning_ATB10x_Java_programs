ee3ae566257a5554b5b93250ba3c37a85f9ea057
ParvathiSanjay <parvathi.gmys@gmail.com>
2025-08-01 18:02:33 +0530

    Constructors2

---
+++ src/OOPS_Class_objects/Greeting.java
@@ -0,0 +1,28 @@
+package OOPS_Class_objects;
+
+public class Greeting {
+    // no param    no return value
+    void m1()
+    {
+        System.out.println("Hello....");
+    }
+
+    // no params   Return value
+    String m2()
+    {
+        return("Hello how are you");
+    }
+
+    // Takes parms    no return value
+    void m3(String name)
+    {
+        System.out.println("Hello..."+name);
+    }
+    // Takes parms and return values
+
+  String m4(String name)
+    {
+        return ("hello...."+name);
+    }
+
+}
